5b00781b5d0f0c8c348075969e7bac955e06a5d6
Vinicius Barboza <viniciusbcomz@gmail.com>
2014-12-14 11:07:46 -0200

    Viabilidade corrigido.

--- src/Viabilidade.m
+++ src/Viabilidade.m
@@ -1,36 +1,40 @@
 function X=Viabilidade(herois, viloes, budget=false)
 
-dataFolder = '../data/';
+  dataFolder = '../data/';
 
-% Carrega as habilidades dos personagens
-L = ListaAtributos(strcat(dataFolder, 'marvel_character.csv'));
+                                % Carrega as habilidades dos personagens
+  L = ListaAtributos(strcat(dataFolder, 'marvel_character.csv'));
 
-% Carrega powergrid dos herois e viloes
-H = L(herois(herois>0), 1:6);
-V = L(viloes, 1:6);
+                                % Carrega powergrid dos herois e viloes
+  H = L(herois(herois>0), 1:6);
+  V = L(viloes, 1:6);
 
-% Soma powergrid dos herois e viloes
-scoreH = sum(H);
-scoreV = sum(V);
+                                % Soma powergrid dos herois e viloes
+  scoreH = sum(H);
+  scoreV = sum(V);
 
-% HTCost
-VTCost = 0;
-for h = 1:length(herois)
-  % Powergrid medio do vilao v
-  pgm = mean(Atributos(L, herois(h)));
-  % Popularidade do vilao v
-  pop = Popularidade(L, herois(h));
-  % VTCost
-  HTCost = HTCost + pgm * pop;
-endfor
+                                % Com budget
+  budgetMax = Inf;
+  if(budget)
+                                % HTCost
+    VTCost = 0;
+    for h = 1:length(herois)
+                                % Powergrid medio do heroi
+      pgm = mean(Atributos(L, herois(h)));
+                                % Popularidade do heroi
+      pop = Popularidade(L, herois(h));
+                                % HTCost
+      HTCost = HTCost + pgm * pop;
+    endfor
 
-% Calcula budget
-budgetMax = max(Budget1(L,herois,viloes), Budget2(L,herois,viloes));
-budget = budget * HTCost;
+                                % Calcula budget
+    budgetMax = max(Budget1(L,herois,viloes), Budget2(L,herois,viloes));
+    budget = budget * HTCost;
+  endif
 
-% Compara media usando apenas inteiros
-if (sum(scoreH * nnz(V) > scoreV * nnz(H)) && (budget < budgetMax)
-  X = 1;
-else
-  X = -1;
-endif
+                                % Compara media usando apenas inteiros
+  if (sum(scoreH * nnz(V) > scoreV * nnz(H)) && (budget < budgetMax)
+      X = 1;
+    else
+      X = -1;
+    endif
