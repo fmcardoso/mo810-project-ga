c79c053652c7f5cac1be8055b1e9b79902157938
Felipe M Cardoso <felipemc88@gmail.com>
2014-12-17 16:31:49 -0200

    Escreve nos dois arquivos automaticamente.

--- src/run.m
+++ src/run.m
@@ -14,16 +14,23 @@ clear all;
 % Lista os arquivos de entrada
 dirlist = dir('../data/input/*.txt');
 
-% Arquivo de escrita final 
+% Arquivo de escrita final - Sem Budget
 outputFileName = '../results/results.txt'
 fileID = fopen(outputFileName,'at');
 fprintf(fileID,'\n------ Inicio execucao ----- \n');
 fclose(fileID);
 
-% Itera nos arquivos de entrada e executa o algoritmo genetico para cada entrada
-for i = 1:length(dirlist)
+% Arquivo de escrita final - Com Budget
+outputBudgetFileName = '../results/results_budget.txt'
+fileID = fopen(outputFileName,'at');
+fprintf(fileID,'\n------ Inicio execucao ----- \n');
+fclose(fileID);
+
+% Itera nos arquivos de entrada e executa o algoritmo genetico para cada entrada 
+for i = 7:length(dirlist)
     viloes = dlmread(strcat('../data/input/', dirlist(i).name));
+    % - Sem budget
 	Principal(viloes, dirlist(i).name, false, outputFileName);
+	% - Com budget
+	Principal(viloes, dirlist(i).name, true, outputBudgetFileName);
 end
-
-
